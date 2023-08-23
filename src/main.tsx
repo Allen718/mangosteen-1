57a141324e5bea3af45bfde1c5a367989feea596
yangyunping <yuy20170301@126.com>
2023-08-23 14:59:08 +0800

    初始化路由 添加路由错误页面

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { ErrorPage } from './pages/error_page/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/1',
+        element: <div>123</div>,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
