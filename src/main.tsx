d949eea7ce093a396cc427c770adf106431a21f9
yyp <yangyunping@millionlab.cn>
2023-08-24 16:55:50 +0800

    尝试使用unocss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import 'virtual:uno.css'
 import { router } from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- src/pages/welcome/welcome_1/Welcome1.tsx
+++ src/pages/welcome/welcome_1/Welcome1.tsx
@@ -3,6 +3,13 @@ import { NavLink } from 'react-router-dom'
 
 export const Welcome1: React.FC = () => {
   return (
-    <div>1 <NavLink to='/welcome/2'>下一页</NavLink></div>
+    <div>
+      <div flex justify-center items-center >
+        <header b-1 b-pink >header</header>
+        <main b-1 b-red grow-1>main</main>
+        <footer b-1 b-blue>footer</footer>
+      </div>
+      <NavLink to='/welcome/2'>下一页</NavLink>
+    </div>
   )
 }
