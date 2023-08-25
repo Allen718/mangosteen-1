000d8c38d9492037a612c795e60dc3b9a6731641
yyp <yangyunping@millionlab.cn>
2023-08-25 15:00:57 +0800

    动画优化

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,7 +1,7 @@
 
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Link, useLocation, useOutlet } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 const linkMap = {
@@ -15,23 +15,32 @@ export const WelcomeLayout: React.FC = () => {
   const location = useLocation()
   const outlet = useOutlet()
   map.current[location.pathname] = outlet
+  const [extraStyle, setExtraStyle] = useState({ position: 'relative' });
   const transitions = useTransition(location.pathname, {
     from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
+    onStart: () => {
+      setExtraStyle({ position: 'absolute' })
+    },
+    onRest: () => {
+      setExtraStyle({ position: 'relative' })
+    },
     config: { duration: 300 }
   })
   return (
     <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
       <header shrink-0 text-center pt-64px>
-        <img src={logo} w-64px />
+        <img src={logo} w-64px h-69px />
         <h1 text="#D4D4EE" text-32px>山竹记账</h1>
       </header>
-      <main shrink-1 grow-1 m-16px >
+      <main shrink-1 grow-1 m-16px relative>
         {transitions((style, pathname) =>
-          <animated.div key={pathname} style={style} bg-white w="100%" h="100%" rounded-8px
-            flex justify-center items-center>
-            {map.current[pathname]}
+          <animated.div key={pathname} style={{ ...extraStyle, ...style }} w="100%" h="100%" flex
+          >
+            <div bg-white rounded-8px flex grow-1 justify-center items-center>
+              {map.current[pathname]}
+            </div>
           </animated.div>
         )}
       </main>
--- src/pages/welcome/welcome_3/Welcome3.tsx
+++ src/pages/welcome/welcome_3/Welcome3.tsx
@@ -4,7 +4,7 @@ import p3 from '../../../assets/images/welcome3.svg'
 export const Welcome3: React.FC = () => {
   return (
     <div text-center>
-      <img src={p3} />
+      <img src={p3} w-130px h-108px />
       <h2 text-32px mt-48px>
         数据可视化 <br />
         收支一目了然
